2f289addff2b3f94dcdcd0041ceabd25ed101ca7
TomoyukiNamura <t.namura017140@gmail.com>
2019-01-06 18:29:22 +0900

    script modify

--- keras_test/scripts/make_df_irregularity.py
+++ keras_test/scripts/make_df_irregularity.py
@@ -10,31 +10,35 @@ from tqdm import tqdm
 import time
 
 
-# データ読み込み
-track = "C"
-file_name = f"track_{track}.csv"
-df_track = pd.read_csv(f"input/{file_name}")
-df_track.head()
+track_list = ["A", "B", "C", "D"]
 
 
-columns = "高低左"
-df_irregularity = []
-milage_list = list(df_track.loc[:,"キロ程"].unique())
-
-print("行：日付id, 列：キロ程　のデータ作成")
-time.sleep(0.5)
-for milage in tqdm(milage_list):
-    # 対象キロ程のデータを取得
-    tmp_df = df_track[df_track.loc[:,"キロ程"]==milage].loc[:,[columns]]
+for track in track_list:
+    # データ読み込み
+    file_name = f"track_{track}.csv"
+    df_track = pd.read_csv(f"input/{file_name}")
+    df_track.head()
     
-    # インデックスを初期化
-    tmp_df = tmp_df.reset_index(drop=True)
     
-    # リネームしてアペンド
-    df_irregularity.append(tmp_df.rename(columns={columns: f"m{milage}"}))
+    columns = "高低左"
+    df_irregularity = []
+    milage_list = list(df_track.loc[:,"キロ程"].unique())
+    
+    print("行：日付id, 列：キロ程　のデータ作成")
+    time.sleep(0.5)
+    for milage in tqdm(milage_list):
+        # 対象キロ程のデータを取得
+        tmp_df = df_track[df_track.loc[:,"キロ程"]==milage].loc[:,[columns]]
+        
+        # インデックスを初期化
+        tmp_df = tmp_df.reset_index(drop=True)
+        
+        # リネームしてアペンド
+        df_irregularity.append(tmp_df.rename(columns={columns: f"m{milage}"}))
+    
+    df_irregularity = pd.concat(df_irregularity, axis=1)
+    
+    # 保存
+    df_irregularity.to_csv(f"input/irregularity_{track}.csv",index=False,header=True)
 
-df_irregularity = pd.concat(df_irregularity, axis=1)
-
-# 保存
-df_irregularity.to_csv(f"input/irregularity_{track}.csv",index=False,header=True)
-
+    time.sleep(5)
